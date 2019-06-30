caa2bb61e020fbac80231acb7fdbdb1d6fd8d002
clement <clement.demonchy@jobteaser.com>
2019-06-30 14:34:28 +0200

    Working again yess

--- crawling/crawl.py
+++ crawling/crawl.py
@@ -61,15 +61,16 @@ def crawl_research(research):
     search_bar = browser.find_element_by_id("searchByCaseName")
     search_bar.click() 
     search_bar.send_keys(research)
-    time.sleep(1)
+    # need some time for autocomplete to finish
+    time.sleep(2)
     autocomplete_list = browser.find_element_by_id("ui-id-1")
-    logging.info(autocomplete_list)
-    items = autocomplete_list.find_elements(By.TAG_NAME,"li")
-    logging.info(items)
+    items = autocomplete_list.find_elements_by_tag_name("li")
+    logging.info(f"Found {len(items)} matching proprositions for {research}")
     list_url_to_crawl = []
-    logging.debug(f"Noting found {len(items)} result for {research}")
     for item in items:
-        item.click
+        logging.info(f"loading info for {item.text} case")
+        # Got exception "Element is not currently visible and may not be manipulated" using item.click so we need to bypas and send javascript request directly
+        browser.execute_script("arguments[0].click();", item);
         # need some time to load result
         time.sleep(3)
         correct_link = browser.find_element(By.XPATH,"/html/body/main/section[1]/div/div/div[2]/div/div/div[2]/div[3]/div[1]/div/table/tbody/tr/td[2]/a")
@@ -77,13 +78,17 @@ def crawl_research(research):
         list_url_to_crawl.append(str(correct_link.get_attribute('href')))
     browser.quit()
     display.stop()
+    logging.info(list_url_to_crawl)
     return list_url_to_crawl
 
-if __name__ == '__main__':
-    list_result = crawl_research("bulgaria")
+def crawling_site(research):
+    list_result = crawl_research(research)
     if list_result:
         process = CrawlerProcess()
         process.crawl(QuotesSpider, start_urls=list_result)
         process.start()
     else:
-        logging.debug("Nothing to crawl")
+        logging.debug(f"Nothing to crawl for {research}")
+
+if __name__ == '__main__':
+    crawling_site("bulgaria")
