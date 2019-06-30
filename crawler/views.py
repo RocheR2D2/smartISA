a567605ca648532303011c0b07a0b29a9879fe56
clement <clement.demonchy@jobteaser.com>
2019-06-30 21:23:03 +0200

    Fix returned endpoint django

--- crawler/views.py
+++ crawler/views.py
@@ -9,14 +9,14 @@ scrapyd = ScrapydAPI('http://scrapyd:6800')
 
 def crawling(request, research):
     list_result = crawl.crawl_research(research)
-    task_status = "Failed"
+    task = {"status":"Failed"}
     if list_result:
         try:
             task = scrapyd.schedule('default', 'quotes', start_urls=list_result, domain="investmentpolicy.unctad.org")
+            task={'task_id': task, 'status': 'started' }
             logging.info(task)
-            task_status="started"
         except Exception as e:
             logging.exception(e)
     else:
         logging.debug(f"Nothing to crawl for {research}")
-    return {"task_status": task_status, "status_code":200}
+    return JsonResponse(task)
